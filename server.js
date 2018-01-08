7400dcd8f31ba91aa5876742c7a7573e88299d1e
amit <amitnegi0195@gmail.com>
2018-01-08 14:33:41 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express= require('express');
+const hbs = require('hbs');
+
+var app=express();
+
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+app.get('/',function(req,res)
+{
+  res.render('home.hbs',
+  {
+    welcome_message:'welcome to Handle bar example',
+    ti:'2nd handelBar ',
+    year:new Date().getFullYear()
+  });
+});
+
+app.get('/bad',function(req,res)
+{
+  res.send({
+    message:'error found'
+  });
+});
+app.get('/about',function(req,res)
+{
+  res.render('temp.hbs',
+  {
+    ti:'1st handelBar ',
+    year:new Date().getFullYear()
+  });
+});
+
+app.listen(3000,function()
+{
+  console.log(' server started on port 3000');
+});
